80737a8d81c661a7d8f1234fe50857bc12b43dac
Jeff Lawson <jeff.lawson@flightaware.com>
2011-10-31 17:12:03 -0500

    add some more test cases

--- tests/dict.tcl
+++ tests/dict.tcl
@@ -6,12 +6,34 @@ package require yajltcl
 set failed 0
 
 proc dotest {raw} {
-	set expected [::json::json2dict $raw]
-	set actual [::yajl::json2dict $raw]
-	puts "Input: $raw"
+	#set count 10000
+	set count 1
+
+	# do the tcllib method
+	set start [clock clicks]
+	for {set i 0} {$i < $count} {incr i} {
+		set expected [::json::json2dict $raw]
+	}
+	set end [clock clicks]
+	if {$count > 1} {
+		puts "tcllib took [expr $end-$start] clicks"
+	}
+
+	# do the yajl method.
+	set start [clock clicks]
+	for {set i 0} {$i < $count} {incr i} {
+		set actual [::yajl::json2dict $raw]
+	}
+	set end [clock clicks]
+	if {$count > 1} {
+		puts "yajl took [expr $end-$start] clicks"
+	}
+
+	# compare the results.
 	puts "Expected: $expected"
 	puts "Actual: $actual"
 	if {$actual != $expected} {
+		puts "Input: $raw"
 		puts "FAILED"
 		incr ::failed
 	} else {
@@ -26,6 +48,7 @@ dotest {{"moo": [123, 4576, 789], "cow": "moooo"}}
 dotest {{"moo": [123, 4576, 789], "cow": "moooo", "complex": {"alfa" : "a", "bravo": "b", "charlie" : "c", "delta"  : "d" }}}
 dotest {{"moo": "cow", "pig": "oink", "rabbit" : null}}
 
+# example from the yajl-tcl README file.
 dotest {{
     "type": "FeatureCollection",
 	"features": [
@@ -57,6 +80,47 @@ dotest {{
 				 }
 				 ]
 }}
+
+# example from "man n json" (tcllib)
+dotest {[
+		 {
+              "precision": "zip",
+              "Latitude":  37.7668,
+              "Longitude": -122.3959,
+              "Address":   "",
+              "City":      "SAN FRANCISCO",
+              "State":     "CA",
+              "Zip":       "94107",
+              "Country":   "US"
+		 },
+		 {
+              "precision": "zip",
+              "Latitude":  37.371991,
+              "Longitude": -122.026020,
+              "Address":   "",
+              "City":      "SUNNYVALE",
+              "State":     "CA",
+              "Zip":       "94085",
+              "Country":   "US"
+		 }
+		 ]
+}
+
+# example from "man n json" (tcllib)
+dotest {{
+	"Image": {
+               "Width":  800,
+               "Height": 600,
+               "Title":  "View from 15th Floor",
+		"Thumbnail": {
+                   "Url":    "http://www.example.com/image/481989943",
+                   "Height": 125,
+                   "Width":  "100"
+		},
+		"IDs": [116, 943, 234, 38793]
+	}
+}}
+
 
 
 exit $failed
