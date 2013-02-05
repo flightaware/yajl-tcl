988f91c9d06e8b2b2a858ab286c1c06228d347dc
Jeff Lawson <jeff.lawson@flightaware.com>
2013-02-05 15:47:55 -0600

    add script to simplify version number changes.

---
+++ update_ver.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# This script simplifies the process of incrementing all version numbers for a new release.
+
+NEWVER="1.3"
+
+perl -p -i -e "s/^(AC_INIT\\(\\[[a-z_]+\\],) \\[[0-9.]+\\]/\\1 \\[$NEWVER\\]/" configure.in
+
+perl -p -i -e "s/^(\*Version) [0-9.]+(\*)/\\1 $NEWVER\\2/" README.md
+
+autoconf
