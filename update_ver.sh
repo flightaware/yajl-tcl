8442b1d327b704d0501fe799f2cba62725496c49
Jeff Lawson <jeff.lawson@flightaware.com>
2016-06-21 19:47:03 +0000

    increment to version 1.6.1

--- update_ver.sh
+++ update_ver.sh
@@ -2,7 +2,7 @@
 
 # This script simplifies the process of incrementing all version numbers for a new release.
 
-NEWVER="1.6"
+NEWVER="1.6.1"
 
 perl -p -i -e "s/^(AC_INIT\\(\\[[a-z_]+\\],) \\[[0-9.]+\\]/\\1 \\[$NEWVER\\]/" configure.in
 
